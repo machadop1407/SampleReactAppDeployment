30e35361793c90f7d9d1e49f888cfc7190d78444
Pedro Machado <machadop1407@gmail.com>
2020-10-16 18:19:32 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+
+function App() {
+  useEffect(()=> {
+    
+  })
+  return (
+    <div>
+      dasdfasdf
+    </div>
+  );
+}
+
+export default App;
